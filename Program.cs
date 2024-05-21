fe9c16ca59235a1b3f27311efbe49a53e56b1136
poojithathimegowda <poojithagowda98@gmail.com>
2024-05-21 18:37:33 +0200

    Few samll changes

--- Program.cs
+++ Program.cs
@@ -57,8 +57,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
+app.MapControllerRoute( name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
